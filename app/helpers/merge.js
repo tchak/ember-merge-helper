34827a466a088729c19fedd844e69ae33cd35648
Paul Chavard <git@paul.chavard.net>
2018-04-30 16:14:17 +0300

    Use @ember.assign

--- app/helpers/merge.js
+++ app/helpers/merge.js
@@ -1,5 +1,6 @@
 import { helper } from '@ember/component/helper';
+import { assign } from '@ember/polyfills';
 
 export default helper(function(hashes, hash) {
-  return Object.assign(Object.create(null), ...hashes, hash);
+  return assign(Object.create(null), ...hashes, hash);
 });
